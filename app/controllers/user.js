18cb8a73b5a645d7cb4aa748dc299b679bf1eb35
privateOmega <0006mathew@gmail.com>
2017-04-19 10:01:05 +0530

    com

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -209,6 +209,7 @@ exports.sos = (req, res) => {
         res.json({ success: true, message: 'An e-mail has been sent to police with further instructions.' });
         done(err, 'done');
       });
+      res.json({success:true});
     }
   ], function(err) {
     if (err) console.log(err);
