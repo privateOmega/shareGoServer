73b62ab71ff1c143464105f234eb52fb71214218
privateOmega <0006mathew@gmail.com>
2017-04-28 09:54:25 +0530

    m

--- app/controllers/trip.js
+++ app/controllers/trip.js
@@ -32,7 +32,7 @@ exports.getTripDetails = (req,res,next) =>{
   else if(req.body.role== "pax"){
     console.log("pax role");
           
-      passengertrip.findOne({$and:[{_id:req.body._id},{status:'OTG'}]}, (err, existingRide2) => {
+      passengertrip.findOne({_id:req.body._id}}, (err, existingRide2) => {
         if (err) { return next(err); }
         if (existingRide2) {
           console.log("Found existing ride2");
